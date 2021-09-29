a637aa3bc938c8ca8c7c0ad7dbede8a678fb7984
Gayatri-Yawalkar <89961118+Gayatri-Yawalkar@users.noreply.github.com>
2021-09-29 12:16:11 +0530

    Delete HelloLineComparison.java

--- HelloLineComparison.java
+++
@@ -1,6 +0,0 @@
-package com.bridgelabz.linecomparison;
-public class HelloLineComparison {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Program");
-	}
-}
